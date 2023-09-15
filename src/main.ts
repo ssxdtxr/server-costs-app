c66463c10681f1c7d8d28c62a4d749b3f47c3924
ssxdtxr <tereskinegor68@gmail.com>
2023-09-15 11:13:26 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.setGlobalPrefix('api')
-  await app.listen(7000)
+  app.listen(7000)
 }
 
 bootstrap()
